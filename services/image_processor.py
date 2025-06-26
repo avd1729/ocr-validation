ad92bc26f463a20c29aa1ffee1c2dace926f7972
Aravind <aravind19112003@gmail.com>
2025-06-26 10:42:08 +0530

    optimize face match

--- services/image_processor.py
+++ services/image_processor.py
@@ -4,17 +4,19 @@ from services.config import PDF_DPI
 
 def prepare_images_sync(pdf_data):
     try:
-        images = convert_from_bytes(pdf_data, dpi=PDF_DPI)
-        if len(images) < 3:
+        images = convert_from_bytes(pdf_data, dpi=100, first_page=2, last_page=3)
+        if len(images) < 2:
             return None, None
-        
+
         img2 = BytesIO()
         img3 = BytesIO()
-        images[1].save(img2, format="JPEG")
-        images[2].save(img3, format="JPEG")
+
+        images[0].convert("RGB").save(img2, format="JPEG", quality=75)
+        images[1].convert("RGB").save(img3, format="JPEG", quality=75)
+
         img2.seek(0)
         img3.seek(0)
-        
+
         return img2.read(), img3.read()
     except Exception as e:
         print("Image preparation error:", e)
